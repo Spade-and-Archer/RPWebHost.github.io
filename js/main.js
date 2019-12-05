28219d48514fd24d434a2577060bdc593837577e
Andy Tewfik <ssploder@gmail.com>
2019-12-05 15:48:06 -0500

    Okay, the features tab is now working. Still need to add a couple more and add images, but it works structurally

--- js/main.js
+++ js/main.js
@@ -19,32 +19,71 @@ function ClearActiveTabs() {
 }
 
 const features = [
-  {FeatureTitle : "Painting!", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
-  {FeatureTitle : "Custom Map Markers", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
-  {FeatureTitle : "Loot Highlighting", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
-  {FeatureTitle : "Guitar Macro", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
-  {FeatureTitle : "Minimap and Wayfinding", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+  {FeatureTitle : "Painting!",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Allows you to create paintings from image files in under 5 minutes",
+    FeatureStatus : "done"},
+
+  {FeatureTitle : "Custom Map Markers",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Records the position of your screenshots and allows you to add notes to your map that will be overlayed on your regular map whenever you hit G",
+    FeatureStatus : "soon"},
+
+  {FeatureTitle : "Loot Highlighting",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Will highlight red any loot in your inventory that is lower value than the loot chest you are looking at and highlight green any high-value loot you should prioritize",
+    FeatureStatus : "soon"},
+
+  {FeatureTitle : "Guitar Macro",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Make it so you can jam out to sick tunes on your way to your target",
+    FeatureStatus : "soon"},
+
+  {FeatureTitle : "Minimap and Wayfinding",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Overlaying a small map and compass in the corner of the screen to help you see your immediate surroundings without having to hit G",
+    FeatureStatus : "soon"},
+
+  {FeatureTitle : "Player tracking",
+    FeatureImg : "img/LayerOfDetail 1.jpg",
+    FeatureDescription : "Get alerts when an enemy clan comes online, keep track of your enemies changing usernames, and view trends in your target's online hours so you can attack when they least expect it",
+    FeatureStatus : "soon"},
 
 ];
+
 function LoadEachFeature() {
+  let fHolder = document.createElement("div");
+  fHolder.id = "fholder";
+  document.getElementById("PageBody").appendChild(fHolder);
   for (let i = 0; i < features.length; i++) {
     let newElement = document.createElement("div");
     newElement.id = "featurebox-" + i.toString();
-    document.getElementById("PageBody").appendChild(newElement);
+    newElement.className = "featurebox";
+    fHolder.appendChild(newElement);
+    //newElement.addEventListener("load", function(){console.log("yay")});
     //$("#PageBody").add(newElement);
     $("#featurebox-" + i.toString()).load("html/FeatureCard.html");
-    $("#fc-blank-title").innerText = features[i].FeatureTitle;
-    $("#fc-blank-title").id = "#fc-title-" + i.toString();
 
-    $("#fc-blank-icon").innerText = features[i].FeatureStatus;
-    $("#fc-blank-icon").id = "#fc-icon-" + i.toString();
-
-    $("#fc-blank-description").innerText = features[i].FeatureDescription;
-    $("#fc-blank-description").id = "#fc-description-" + i.toString();
-
-    $("#fc-blank-image").style = "url(" + features[i].FeatureImg + ")";
-    $("#fc-blank-image").id = "#fc-image-" + i.toString();
-
+    // $("#fc-blank-title").innerText = features[i].FeatureTitle;
+    // $("#fc-blank-title").id = "#fc-title-" + i.toString();
+    //
+    // //$("#fc-blank-icon").innerText = features[i].FeatureStatus;
+    // //$("#fc-blank-icon").id = "#fc-icon-" + i.toString();
+    //
+    // $("#fc-blank-description").innerText = features[i].FeatureDescription;
+    // $("#fc-blank-description").id = "#fc-description-" + i.toString();
+    //
+    // $("#fc-blank-image").style = "url(" + features[i].FeatureImg + ")";
+    // $("#fc-blank-image").id = "#fc-image-" + i.toString();
+    //
+    // $("#status-" + features[i].FeatureStatus).addClass("feature-active-icon");
+    // $("#status-" + features[i].FeatureStatus).id = "#status-" + features[i].FeatureStatus + i.toString();
+    //
+    // $("#status-done").addClass("feature-active-icon");
+    // $("#status-alarm").remove();
+    // $("#status-later").remove();
+    //$(selector).removeClass("feature-hidden-icon");
+    //console.log(selector);
 
 
   }
