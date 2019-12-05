21c0b94b2bfcc3c26eaecbb228f4b343e832016d
Andy Tewfik <ssploder@gmail.com>
2019-12-05 13:45:33 -0500

    Made a feature showcase. Committing before I break everything in 6 seconds and have to revert

--- js/main.js
+++ js/main.js
@@ -8,3 +8,79 @@ function OnBodyLoad(){
     }
   }
 }
+function ClearActiveTabs() {
+  let TabElements = document.getElementsByClassName("develop-tab");
+  for(var i = 0; i < TabElements.length; i++){
+
+    TabElements[i].classList.remove("develop-tab--active");
+    if(TabElements[i].children.length > 0)
+      TabElements[i].children[0].hidden = true;
+  }
+}
+
+const features = [
+  {FeatureTitle : "Painting!", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+  {FeatureTitle : "Custom Map Markers", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+  {FeatureTitle : "Loot Highlighting", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+  {FeatureTitle : "Guitar Macro", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+  {FeatureTitle : "Minimap and Wayfinding", FeatureImg : "img/LayerOfDetail 1.jpg", FeatureDescription : "Allows you to create paintings from image files in under 5 minutes", FeatureStatus : "Done"},
+
+];
+function LoadEachFeature() {
+  for (let i = 0; i < features.length; i++) {
+    let newElement = document.createElement("div");
+    newElement.id = "featurebox-" + i.toString();
+    document.getElementById("PageBody").appendChild(newElement);
+    //$("#PageBody").add(newElement);
+    $("#featurebox-" + i.toString()).load("html/FeatureCard.html");
+    $("#fc-blank-title").innerText = features[i].FeatureTitle;
+    $("#fc-blank-title").id = "#fc-title-" + i.toString();
+
+    $("#fc-blank-icon").innerText = features[i].FeatureStatus;
+    $("#fc-blank-icon").id = "#fc-icon-" + i.toString();
+
+    $("#fc-blank-description").innerText = features[i].FeatureDescription;
+    $("#fc-blank-description").id = "#fc-description-" + i.toString();
+
+    $("#fc-blank-image").style = "url(" + features[i].FeatureImg + ")";
+    $("#fc-blank-image").id = "#fc-image-" + i.toString();
+
+
+
+  }
+}
+
+function LoadFeatures(){
+  ClearActiveTabs();
+  document.getElementById("features").classList.add("develop-tab--active");
+  document.getElementById("features").children[0].hidden = false;
+  //$("#PageBody").load("html/Features.html");
+
+  LoadEachFeature();
+
+
+  // for(let i = 0; i < features.length; i ++){
+  //   let newElement = document.createElement("div")
+  //   newElement.id = "featurebox" + i.toString();
+  //   $("#PageBody").add(newElement);
+  //   $("#featurebox" + i.toString()).load("html/FeatureCard.html");
+  //   $("#fc-blank-title").innerText = features[i].FeatureTitle;
+  //   $("#fc-blank-title").id = "#fc-title-" + i.toString();
+  //
+  //   $("#fc-blank-icon").innerText = features[i].FeatureStatus;
+  //   $("#fc-blank-icon").id = "#fc-icon-" + i.toString();
+  //
+  //   $("#fc-blank-description").innerText = features[i].FeatureDescription;
+  //   $("#fc-blank-description").id = "#fc-description-" + i.toString();
+  //
+  //   $("#fc-blank-image").style = "url(" + features[i].FeatureImg + ")";
+  //   $("#fc-blank-image").id = "#fc-image-" + i.toString();
+  //
+  //   document.getElementById("PageBody").appendChild(newElement);
+  //}
+}
+function LoadAbout(){
+  ClearActiveTabs();
+  document.getElementById("about").classList.add("develop-tab--active");
+  document.getElementById("about").children[0].hidden = false;
+}
